d20db998807055c51b4bcad57863f9664fc41857
brittLiban <britt.liban@student.greenriver.edu>
2025-06-21 11:27:24 -0700

    Magic Link works but NOT the normal sign in- Added login feature!

---
+++ front-end/src/app/login/page.js
@@ -0,0 +1,23 @@
+'use client';
+
+import { Auth } from '@supabase/auth-ui-react';
+import { ThemeSupa } from '@supabase/auth-ui-shared';
+import supabase from '../lib/supabaseClient'
+
+export default function LoginPage() {
+  return (
+    <div style={{ maxWidth: '400px', margin: '0 auto', marginTop: '3rem' }}>
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={[]}
+        localization={{
+          variables: {
+            sign_in: { email_label: 'Email address' },
+          },
+        }}
+        magicLink={true}
+      />
+    </div>
+  );
+}
